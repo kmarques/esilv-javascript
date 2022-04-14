860684e96afb85a03b54ce377c00855983fcec7b
Karl MARQUES <kmarques@vetixy.com>
2022-04-14 14:51:12 +0200

    BIN2: exo webapi

---
+++ BIN2/exo_webapi.js
@@ -0,0 +1,3 @@
+// Générer un tableau de 5 par 5 sous la balise "root" avec une valeur par défaut "Default"
+// Au clique sur une case transformer la valeur par un input contanenant la valeur
+// Dès qu'on sort de la case, remplacer l'input par la nouvelle valeur

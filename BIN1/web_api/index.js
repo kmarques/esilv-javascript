29f20ac633bf3f4a9615ff45480bb06b5d1eb094
Karl MARQUES <karl@kmarques.dev>
2025-04-04 11:28:45 +0200

    BIN2: web api

--- BIN1/web_api/index.js
+++ BIN1/web_api/index.js
@@ -1,13 +1,43 @@
 const root = document.getElementById("root");
 const table = document.createElement("table");
 const tbody = document.createElement("tbody");
-for (let rowIndex = 0; rowIndex < 5; rowIndex++) {
+const data = JSON.parse(localStorage.getItem("data") || "{}");
+
+function onBlur(event) {
+  const input = event.currentTarget;
+  const text = input.value;
+  const textNode = document.createTextNode(text);
+  const td = input.parentNode;
+  data[td.dataset.coordinate] = text;
+  localStorage.setItem("data", JSON.stringify(data));
+  localStorage.setItem(td.dataset.coordinate, text);
+  td.replaceChild(textNode, input);
+  td.addEventListener("click", onClick);
+  input.removeEventListener("blur", onBlur);
+}
+
+function onClick(event) {
+  const td = event.currentTarget;
+  const textNode = td.childNodes[0];
+  const text = textNode.textContent;
+  const input = document.createElement("input");
+  input.value = text;
+  td.replaceChild(input, textNode);
+  input.focus();
+  input.addEventListener("blur", onBlur);
+  td.removeEventListener("click", onClick);
+}
+
+for (let rowIndex = 0; rowIndex < 20; rowIndex++) {
   const tr = document.createElement("tr");
-  for (let colIndex = 0; colIndex < 5; colIndex++) {
+  for (let colIndex = 0; colIndex < 20; colIndex++) {
     const td = document.createElement("td");
-    const text = document.createTextNode("Default");
+    td.dataset.coordinate = `${rowIndex},${colIndex}`;
+    const savedValue = localStorage.getItem(td.dataset.coordinate) || "Default";
+    const text = document.createTextNode(savedValue);
     td.appendChild(text);
     tr.appendChild(td);
+    td.addEventListener("click", onClick);
   }
   tbody.appendChild(tr);
 }
--- BIN2/web_api/index.js
+++ BIN2/web_api/index.js
@@ -1,12 +1,48 @@
+const rootDiv = document.getElementById("root");
 const table = document.createElement("table");
 const tbody = document.createElement("tbody");
+table.appendChild(tbody);
 
-const MAX_ROW = 50000;
-const MAX_COL = 50000;
+const MAX_ROW = 50;
+const MAX_COL = 50;
+
+function onBlur(event) {
+  const input = event.currentTarget;
+  const text = input.value;
+  const textNode = document.createTextNode(text);
+  const td = input.parentNode;
+  td.replaceChild(textNode, input);
+  localStorage.setItem(td.dataset.coord, text);
+  input.removeEventListener("blur", onBlur);
+  td.addEventListener("click", onClick);
+}
+
+function onClick(event) {
+  const td = event.currentTarget;
+  const textNode = td.childNodes[0];
+  const text = textNode.textContent;
+  const input = document.createElement("input");
+  input.value = text;
+  td.replaceChild(input, textNode);
+  input.focus();
+  input.addEventListener("blur", onBlur);
+  td.removeEventListener("click", onClick);
+}
+
 for (let rowIndex = 0; rowIndex < MAX_ROW; rowIndex++) {
   const tr = document.createElement("tr");
+  tbody.appendChild(tr);
   for (let colIndex = 0; colIndex < MAX_COL; colIndex++) {
     const td = document.createElement("td");
-    const text = document.createTextNode("Default");
+    const coord = `${rowIndex},${colIndex}`;
+    td.dataset.coord = coord;
+    const text = document.createTextNode(
+      localStorage.getItem(coord) || "Default"
+    );
+    tr.appendChild(td);
+    td.appendChild(text);
+    td.addEventListener("click", onClick);
   }
 }
+
+rootDiv.appendChild(table);

c1be4cb99da0d1783a9bdee84fd26ca97df86e88
Karl MARQUES <karl@kmarques.dev>
2026-01-27 15:56:42 +0100

    first commit

---
+++ BIC1/exemple_for.js
@@ -0,0 +1,28 @@
+const voiture = {
+  marque: "Toyota",
+  modele: "Corolla",
+  annee: 2020,
+  afficherDetails: function () {
+    console.log(
+      `Marque: ${this.marque}, Modèle: ${this.modele}, Année: ${this.annee}`,
+    );
+  },
+};
+
+const Marques = ["Toyota", "Honda", "Ford", "Chevrolet", "Nissan"];
+
+for (let marque of Marques) {
+  console.log(marque);
+}
+for (let marqueIndex in Marques) {
+  console.log(marqueIndex);
+}
+
+for (let key in voiture) {
+  console.log(key);
+}
+
+//  Impossible avec un objet
+// for (let value of voiture) {
+//   console.log(value);
+// }
